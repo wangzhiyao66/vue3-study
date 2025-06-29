7f8972c096b5f08c12a79b6f15ca53f85f542355
wangzhiyao <1217167460@qq.com>
2025-06-29 13:42:40 +0800

    feat: 解构 url 参数：Object.fromEntries(new URL(url3).searchParams))

--- src/views/ProblemView.vue
+++ src/views/ProblemView.vue
@@ -18,8 +18,36 @@ let url = 'https://example.com/page?param1=value1&param2=value2'
 let url2 = 'http://www.wheelsfactory.cn/detail?id=55&name=张三'
 let url3 = 'http://www.wheelsfactory.cn/detail?id=55&name=张三&age=20'
 console.log('url2 ==  ', getParam(url))
+/**
+ * URL {
+ *  href: 'https://example.com/page?param1=value1&par·am2=value2',
+ *  origin: 'https://example.com',
+ *  protocol: 'https:',
+ *  username: '',
+ *  password: '',
+ *  host: 'example.com',
+ *  hostname: 'example.com',
+ *  port: '',
+ *  pathname: '/page',
+ *  search: '?param1=value1&param2=value2',
+ * }
+ *  searchParams: URLSearchParams { 'param1' => 'value1', 'param2' => 'value2' },
+ *
+*/
 console.log('url2 ==  ', getParam(url).searchParams.get('param1'))
+/**
+ * value1
+ */
 console.log('url3 ==  ', getQueryParams(new URL(url3).search))
-console.log('url4 ==  ', Object.fromEntries(new URLSearchParams(new URL(url3).search)))
+/**
+ * { id: '55', name: '张三', age: '20' }
+ */
+console.log('url4 ==  ', Object.fromEntries(new URLSearchParams(url3)))
+/**
+ * {http://www.wheelsfactory.cn/detail?id: '55', name: '张三', age: '20'}
+ */
 console.log('url5 ==  ', Object.fromEntries(new URL(url3).searchParams))
+/**
+ * { id: '55', name: '张三', age: '20' }
+ */
 </script>
--- src/views/StoryView.vue
+++ src/views/StoryView.vue
@@ -5,7 +5,7 @@
       <template v-slot:header="scoped">我是插槽头部 {{ scoped.color }}</template>
       <template v-slot:footer="scoped">我是插槽尾部 {{ scoped.name1 }}</template>
     </ChildView>
-    <DraggableComponent />
+    <DraggableComponent ref="drag" />
   </div>
 </template>
 <style lang="less" scoped></style>
@@ -14,10 +14,13 @@ import DraggableComponent from '@/components/DraggableComponent/index.vue'
 import ChildView from '@/components/ChildView.vue'
 import { onMounted, provide, ref } from 'vue';
 
+
 const colorValue = ref<string>('red')
+const drag = ref()
 provide('color', colorValue);
+
 onMounted(() => {
   console.log('provide Test');
-
+  console.log('darg.value', drag.value)
 })
 </script>
